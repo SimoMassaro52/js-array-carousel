222198e692e37540ee6fb63da021066a457f062c
SimoMassaro52 <simonesimomassaro@gmail.com>
2021-12-15 10:22:37 +0100

    push incompleto

--- myscript.js
+++ myscript.js
@@ -15,3 +15,48 @@ di questa fase ci ritroveremo con lo stesso slider, ma costruito dinamicamente a
 JavaScript.
 */
 
+const imgArr = [
+    'img/01.jpg',
+    'img/02.jpg',
+    'img/03.jpg',
+    'img/04.jpg',
+    'img/05.jpg'
+];
+
+const titleArr = [
+    'Svezia',
+    'Svizzera',
+    'Gran Bretagna',
+    'Germania',
+    'Paradise'
+];
+
+const textArr = [
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+    'Lorem ipsum',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+];
+
+let mainContent = '';
+let thumbs = '';
+
+for (let i = 0; i < imgArr.length; i++) {
+    mainContent += `
+    <div id="img-${[i+1]}">
+        <img src="${imgArr[i]}">
+        <div class="position-relative" id="textbox"><h5 id="title">${titleArr[i]}</h5><span id="description">${textArr[i]}</span></div> 
+    </div>`    
+}
+
+document.getElementById('mainbox').innerHTML = mainContent;
+
+for (let i = 0; i < imgArr.length; i++) {
+    thumbs += `
+    <img src="${imgArr[i]}">
+    `
+}
+
+document.getElementById('thumbnails').innerHTML = thumbs;
+
